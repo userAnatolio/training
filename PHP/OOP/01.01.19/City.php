5f2098613fb88da3231bf031b0f06287d1e29889
Anatolio <89261969132@mail.ru>
2019-01-01 20:43:15 +0300

    Хранение объектов в массивах

---
+++ PHP/OOP/01.01.19/City.php
@@ -0,0 +1,31 @@
+﻿<?php
+class City {
+	
+private $name;
+private $population;
+
+public function __construct($name, $population)
+{
+$this -> name = $name;	
+$this -> population = $population;	
+}
+
+public function getName()
+{
+	return $this -> name;
+}
+
+public function getPopulation()
+{
+	return $this -> population . ' чел.';
+}
+
+public function setSalary($population)
+{
+	$this -> population = $population;
+}
+
+}
+
+
+?>
---
+++ PHP/OOP/01.01.19/fileCity.php
@@ -0,0 +1,43 @@
+﻿<?php
+/*
+
+*/
+require_once 'City.php';
+
+$cityArr = [
+new City('Москва', 12500000),
+new City('Таганрог', 350000),
+new City('Ростов-на-Дону', 1130000),
+new City('Воронеж', 1058000),
+new City('Сальск', 58179)
+];
+
+echo '<br>';	
+
+foreach($cityArr as $elem) {
+	echo '<div class="city">';
+echo 'город ' . $elem -> getName(name) . ', население ' . $elem -> getPopulation() . '<br>';
+echo '</div>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: #5d5d5d;
+color: white;
+}
+.city {
+color: yellow;
+background: black;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
