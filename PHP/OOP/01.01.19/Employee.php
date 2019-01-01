95265205224027b8eb489b42df4836879ffe6ae4
Anatolio <89261969132@mail.ru>
2019-01-01 18:59:35 +0300

    Хранение классов в отдельных файлах

---
+++ PHP/OOP/01.01.19/Employee.php
@@ -0,0 +1,34 @@
+﻿<?php
+class Employee {
+private $name;
+private $surname;
+private $patronymic;
+private $salary;
+
+public function __construct($name, $surname, $patronymic, $salary)
+{
+$this -> name = $name;	
+$this -> surname = $surname;	
+$this -> patronymic = $patronymic;
+$this -> salary = $salary;	
+}
+
+public function getName()
+{
+	return $this -> name;
+}
+
+public function getSalary()
+{
+	return $this -> salary . ' $';
+}
+
+public function setSalary($salary)
+{
+	$this -> salary = $salary;
+}
+
+}
+
+
+?>
---
+++ PHP/OOP/01.01.19/file.php
@@ -0,0 +1,66 @@
+﻿<?php
+/*
+Задача 1:
+Сделайте класс Employee, в котором будут следующие свойства: name (имя), surname (фамилия),
+patronymic (отчество) и salary (зарплата).
+Пусть этот класс хранится в отдельном файле.
+
+Задача 2:
+Подключите к файлу index.php класс Employee, создайте два объекта этого класса с произвольными данными,
+выведите на экран сумму их зарплат.
+*/
+
+class Employee {
+private $name;
+private $surname;
+private $patronymic;
+private $salary;
+
+public function __construct($name, $surname, $patronymic, $salary)
+{
+$this -> name = $name;	
+$this -> surname = $surname;	
+$this -> patronymic = $patronymic;
+$this -> salary = $salary;	
+}
+
+public function getName()
+{
+	return $this -> name;
+}
+
+public function getSalary()
+{
+	return $this -> salary . ' $';
+}
+
+public function setSalary($salary)
+{
+	$this -> salary = $salary;
+}
+
+}
+
+$user1 = new Employee('Николай', 'Федотов', 'Александрович', 10000);
+$user2 = new Employee('Федор', 'Федотов', 'Александрович', 20000);
+
+echo $user1 -> getSalary() + $user2 -> getSalary(). '<br>';
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: #5d5d5d;
+color: white;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
