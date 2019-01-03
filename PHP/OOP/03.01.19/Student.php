1884b62231a60ea4c6fc82a049f4b4b4755123e5
Anatolio <89261969132@mail.ru>
2019-01-03 15:19:38 +0300

    Начальные значения свойств в конструкторе

---
+++ PHP/OOP/03.01.19/Student.php
@@ -0,0 +1,34 @@
+﻿<?php
+class Student
+	{
+		private $name;
+		private $course;
+		
+		public function __construct($name)
+		{
+			$this->name = $name;
+			$this->course = 1;
+		}
+		
+		public function getName() 
+		{
+			return  $this -> name;
+		}
+		
+		public function getCourse() 
+		{
+			return $this -> course;
+		}
+		
+		
+		public function addCourse() 
+		{
+			if($this -> course < 5)
+		$this -> course += 1;
+	
+		}
+		
+	}
+
+
+?>
---
+++ PHP/OOP/03.01.19/studentIndex.php
@@ -0,0 +1,36 @@
+﻿<?php
+/*
+
+*/
+require_once 'Student.php';
+
+$student1 = new Student('Коля');
+echo '<div>';
+echo $student1 -> getCourse() . '<br>';
+echo $student1 -> getName() . '<br>';
+echo $student1 -> addCourse() . '<br>';
+echo $student1 -> getCourse() . '<br>';
+echo '</div>';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: #5d5d5d;
+color: white;
+}
+div {
+color: yellow;
+background: black;
+margin-right: 30px;
+padding: 5px;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
