109853726770b6cbad0baaa297d55a0e8d9f506b
Anatolio <89261969132@mail.ru>
2019-01-08 20:49:45 +0300

    Цепочки методов

---
+++ PHP/OOP/08.01.19/User1.php
@@ -0,0 +1,31 @@
+﻿<?php
+	class User1
+	{
+		private $name;
+		private $surname;
+		private $patronymic;
+		
+		public function setName($name)
+		{
+			$this -> name = $name;
+			return $this;
+		}
+		
+		public function setSurname($surname)
+		{
+			$this -> surname = $surname;
+			return $this;
+		}
+		
+		public function setPatronymic($patronymic)
+		{
+			$this -> patronymic = $patronymic;
+			return $this;
+		}
+		
+		public function getFullName()
+		{
+			return substr($this -> name, 0, 1) . substr($this -> surname, 0, 1) . substr($this -> patronymic, 0, 1);
+		}
+	}
+?>
---
+++ PHP/OOP/08.01.19/innerUser1.php
@@ -0,0 +1,52 @@
+﻿<?php
+/*
+Задача 1:
+Не подсматривая в мой код самостоятельно реализуйте такой же класс Arr, методы которого будут вызываться в виде цепочки.
+
+Задача 2:
+Добавьте в класс Arr еще один метод append, который параметром будет принимать массив чисел и добавлять эти числа
+в конец массива, хранящегося в объекте. Предполагается, что методы add и append можно будет использовать в любом порядке:
+<?php
+	echo (new Arr)->add(1)->append([2, 3, 4])->add(5)->getSum();
+?>
+
+Задача 3:
+Сделайте класс User, у которого будут приватные свойства surname (фамилия), name (имя) и patronymic (отчество).
+Эти свойства должны задаваться с помощью соответствующих сеттеров.
+Сделайте так, чтобы эти сеттеры вызывались цепочкой в любом порядке, а самым последним методом в цепочке можно
+было вызвать метод getFullName, который вернет ФИО юзера (первую букву фамилии, имени и отчества).
+Пример:
+<?php
+	echo (new User)->setName('Николай')->setPatronymic('Иванович')
+		->setSurname('Петров')->getFullName(); // выведет 'ПНИ'
+?>
+*/
+
+require_once 'User1.php';
+
+$user1 = new User1();
+	echo $user1 -> setName('Ivan') -> setSurname('Ivanov') -> setPatronymic('Ivanovich') -> getFullName();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: #5d5d5d;
+color: white;
+}
+div {
+color: yellow;
+background: black;
+margin-right: 30px;
+padding: 5px;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
