23281b9594fed27b0cc6267f3a8fbf33ea4b7a0d
Anatolio <89261969132@mail.ru>
2019-01-08 20:28:34 +0300

    Цепочки методов

---
+++ PHP/OOP/08.01.19/Arr.php
@@ -0,0 +1,26 @@
+﻿<?php
+	class Arr
+	{
+		private $numbers = [];
+		
+		public function add($number)
+		{
+			$this->numbers[] = $number;
+			return $this;
+		}
+		
+		public function getSum()
+		{
+			return array_sum($this->numbers);
+		}
+		
+		public function append($arrNum)
+		{
+			foreach($arrNum as $elem)
+			{
+				array_push($this -> numbers, $elem);
+			}
+			return $this;
+		}
+	}
+?>
