cfb5794ab034c4934fe6a8cd917bb6c8a6edb376
Anatolio <89261969132@mail.ru>
2019-01-10 13:02:16 +0300

    Класс как набор методов

---
+++ PHP/OOP/10.01.19/ArrayAvgHelper.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+	class ArrayAvgHelper
+	{
+		
+		public function getAvg1($arr)
+		{
+		return $this ->getSum($arr, 1);
+		}
+		
+		public function getAvg2($arr)
+		{
+		return $this ->getSum($arr, 2);
+		}
+
+		public function getAvg3($arr)
+		{
+		return $this ->getSum($arr, 3);
+		}
+
+		public function getAvg4($arr)
+		{
+		return $this -> getSum($arr, 4);
+		}
+		
+		private function getSum($arr, $power)
+		{
+		$arrSum = 0;
+		foreach($arr as $elem){
+		$arrSum += pow($elem, $power);
+		}
+		return $arrSum;
+		}
+		
+		
+		private function calcSqrt($num, $power)
+		{
+			$sqrtNum = 0;
+		$sqrtNum = sqrt($num, $power);
+		return sqrtNum;
+		}
+
+	}
+?>
---
+++ PHP/OOP/10.01.19/ineerArrayAvgHelper.php
@@ -0,0 +1,34 @@
+﻿<?php
+/*
+Задача:
+Напишите реализацию методов класса ArrayAvgHelper, заготовки методов которого расположены ниже:
+*/
+
+require_once 'ArrayAvgHelper.php';
+$arr = [2, 2];
+$arrayAvgHelper = new ArrayAvgHelper();
+echo $arrayAvgHelper -> getAvg4($arr);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: #5d5d5d;
+color: white;
+}
+div {
+color: yellow;
+background: black;
+margin-right: 30px;
+padding: 5px;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
