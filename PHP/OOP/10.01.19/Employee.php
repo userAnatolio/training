daf775737a274d695555204288ebda0ac9c281e5
Anatolio <89261969132@mail.ru>
2019-01-10 21:10:09 +0300

    Наследование классов

---
+++ PHP/OOP/10.01.19/Employee.php
@@ -0,0 +1,18 @@
+<?php
+
+	class Employee extends User
+	{
+		private $salary;
+		
+		public function setSalary($salary)
+		{
+			$this -> salary = $salary;
+		}
+		
+		public function getSalary()
+		{
+			return $this -> salary;
+		}
+		
+	}
+?>
