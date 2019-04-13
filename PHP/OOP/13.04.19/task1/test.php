844bc99bedb2eb1adf3779e8a987873b3c5edaae
Tolik <89778964651@mail.ru>
2019-04-13 13:54:13 +0300

    static method

---
+++ PHP/OOP/13.04.19/task1/test.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'ArraySumHelper.php';
+$arr = [1, 2, 3, 4];
+echo ArraySumHelper::getSum2($arr);
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Шаблон</title>
+<style>
+body {
+background: black;
+color: white;
+}
+td {
+    border: 1px solid;
+    width: 100px;
+    height: 30px;
+}
+.red {
+	background: black;
+	text-align: center;
+}
+</style>
+</head>
+<body>
+
+</body>
+</html>
